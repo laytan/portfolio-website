0d4e7719ad832b69df6e27881566440712f7d731
Laytan Laats <Laytanlaats@hotmail.com>
2020-03-01 04:57:23 +0100

    Clean up and extract repo collection

---
+++ collections/repos.js
@@ -0,0 +1,183 @@
+const HTMLParser = require('node-html-parser');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const fsp = fs.promises;
+const request = require('request');
+
+async function getRepos(username, accessToken, excludedRepos) {
+  const authenticatedHeaders = githubHeaders(username, accessToken);
+
+  const data = await fetch(`https://api.github.com/users/${username}/repos`, authenticatedHeaders).then(res => res.json());
+
+  // Probably rate limited
+  if(!Array.isArray(data)) {
+    console.error(data);
+    return [];
+  }
+
+  const reposWithNull = await Promise.all(data.map(async (repo) => {
+    // Check if we should add it
+    if(!shouldAdd(repo, excludedRepos)) {
+      return null;
+    }
+  
+    // Get full readme html and image paths
+    const { readme, images } = await parseGithubPage(repo.html_url);
+  
+    // Fetch languages of the repo from github api and parse them into an array of language strings
+    const all_languages = await getLanguages(repo.languages_url, authenticatedHeaders);
+  
+    // Add a node with the repo data from the github api + our custom properties
+    return {
+      ...repo,
+      readme,
+      images,
+      all_languages,
+    }
+  }));
+
+  // Filter out null repos
+  return reposWithNull.filter(repoOrNull => repoOrNull !== null);
+}
+
+/**
+ * Setup headers to sent authenticated github requests via a personal access token
+ * Mainly used to get more requests in before getting rate limited
+ */
+function githubHeaders(username, accessToken) {
+  const authString = `${username}:${accessToken}`;
+  return {
+    headers: {
+      Authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
+    }
+  };
+}
+
+/**
+ * Check if the repo should be added to our collection
+ * @param {Object} repo The repo to check
+ * @param {array} excludedRepos Names of repos to not add regardless
+ */
+function shouldAdd(repo, excludedRepos) {
+  // Don't add forks
+  if(repo.fork) {
+    return false;
+  }
+
+  // Don't add excluded repos
+  if(excludedRepos.includes(repo.name)) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Fetch language endpoint and turn into array of language strings
+ * @param {string} languagesUrl Languages Github API endpoint
+ * @param {object} headers Headers to add to the request
+ */
+async function getLanguages(languagesUrl, headers) {
+  const languages = await fetch(languagesUrl, headers).then(res => res.json());
+  // Github API error (probably rate limit)
+  if(languages.message) {
+    console.error(languages.message);
+    return [];
+  }
+  return Object.keys(languages);
+}
+
+/**
+ * Parses the repos html and returns the readme part
+ * @param {string} url Url of a github repo
+ */
+async function getReadme(url) {
+  const html = await fetch(url).then(res => res.text());
+  const root = HTMLParser.parse(html);
+  return root.querySelector('.markdown-body');
+}
+
+/**
+ * Checks if the image is already downloaded and downloads them otherwise
+ * @param {array} urls Array of image urls to check for locally and download otherwise
+ * @param {string} folder Folder to download into
+ * @returns {array} Array of file names for the local images
+ */
+async function getImages(urls, folder) {
+  return await Promise.all(urls.map(async (url) => {
+    // Get file name out of url
+    // ! If there are 2 files with the same name over 2 repos this won't work
+    const fileName = url.substring(url.lastIndexOf('/') + 1);
+    const downloadLocation = `./src/images/${folder}/${fileName}`;
+    
+    // If the file exists we have downloaded it before, no need to do it again
+    try {
+      // If this is succesfull the file exists
+      await fsp.access(downloadLocation);
+      return downloadLocation;
+    } catch(_) {
+      // If it errors the file does not exist so we download it
+      console.log(`downloading: ${fileName}`);
+      await download(url, downloadLocation);
+      return fileName;
+    }
+  }));
+}
+
+/**
+ * Downloads the image at url into the location
+ * @param {string} url Image url to download
+ * @param {string} location Path to download to
+ */
+function download(url, location) {
+  return new Promise((resolve, reject) => {
+    try {
+      request(url).pipe(fs.createWriteStream(location))
+        .on('close', () => {
+          resolve();
+        }
+      );
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
+/**
+ * Checks for relative url and turns it into an absolute url
+ * @param {Object} imgE Img element to get src from
+ */
+function absoluteGithubSrc(imgEl) {
+  const src = imgEl.getAttribute('src');
+  // If the src is already an absolute url return the original src
+  if(src.includes('https://')) {
+    return src;
+  }
+  // Turn the relative url into a absolute github url
+  return `https://github.com${imgEl.getAttribute('src')}`;
+}
+
+/**
+ * Parses the github page for its readme and the images in that readme
+ * @param {string} url Github repo URL
+ * @returns { readme: string, images: string } Images are local filenames in /src/images/github
+ */
+async function parseGithubPage(url) {
+  const readme = await getReadme(url);
+
+  // Some repos don't have a readme, return empty results
+  if(!readme) {
+    return { readme: '', images: [] };
+  }
+
+  // All image urls in the readme
+  const readmeImageURLs = readme.querySelectorAll('img').map(absoluteGithubSrc);
+
+  // Download all the images and return their paths
+  // * Downloading is done because we want to benefit from gridsome's g-image component *
+  const readmeImagePaths = await getImages(readmeImageURLs, 'github');
+
+  return { readme: readme.innerHTML, images: readmeImagePaths };
+}
+
+module.exports = getRepos;
--- gridsome.server.js
+++ gridsome.server.js
@@ -1,98 +1,22 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-const HTMLParser = require('node-html-parser');
-const fetch = require('node-fetch');
-const fs = require('fs');
-const request = require('request');
+/**
+ * Hook into gridsome and add our custom github repo collection
+ */
 require('dotenv').config();
+const getRepos = require('./collections/repos');
 
-const REPO_URL = 'https://api.github.com/users/laytan/repos';
-
-const auth = 'laytan:' + process.env.PERSONAL_ACCESS_TOKEN;
-const buff = new Buffer(auth);
-const base64 = buff.toString('base64');
-const headers = {
-  Authorization: 'Basic ' + base64,
-};
-
-async function parseGithubPage(url) {
-  return await fetch(url).then(res => res.text()).then(async (html) => {
-    const root = HTMLParser.parse(html);
-    const readme_html = root.querySelector('.markdown-body');
-    if(!readme_html) {
-      return;
-    }
-
-    // Get all image urls and turn relative urls into full urls
-    const imageEls = readme_html.querySelectorAll('img');
-    const image_urls = imageEls.map(el => {
-      const src = el.getAttribute('src');
-      if(src.includes('https://')) {
-        return src;
-      }
-      return `https://github.com${el.getAttribute('src')}`;
-    });
-
-    // Download all images locally
-    const images = await Promise.all(image_urls.map(async (url) => {
-      try {
-        const fileName = url.substring(url.lastIndexOf('/') + 1);
-
-        const dest = `./src/images/github/${fileName}`;
-        if(fs.existsSync(dest)) {
-          return fileName;
-        }
-        console.log(`downloading image: ${fileName}`);
-        request(url).pipe(fs.createWriteStream(dest)).on('close', () => {
-          return fileName;
-        });
-      } catch(e) {
-        console.error(e);
-      }
-    }));
-    return { readme: readme_html.innerHTML || '', images: images || [] };
-  });
-}
+const EXCLUDED_REPOS = ['portfolio-website'];
+const GITHUB_USERNAME = 'laytan';
 
 module.exports = function (api) {
-  api.loadSource(async ({ addCollection }) => {
-    const data = await fetch(REPO_URL, { headers }).then(res => res.json()).catch(console.error);
-    if(!Array.isArray(data)) {
-      console.error(data);
-      return;
-    }
-
+  api.loadSource(async ({ addCollection }) => {    
+    // Initialize a collection to add our reposz to
     const collection = addCollection({
       typeName: 'Repo',
     });
 
-    for(const repo of data) {
-      // Don't add forks
-      if(repo.fork) {
-        continue;
-      }
-
-      // Parse readme and images
-      const parsed = await parseGithubPage(repo.html_url);
-
-      const node = {
-        ...repo,
-        ...parsed,
-        all_languages: await fetch(repo.languages_url, { headers }).then(res => res.json()).then(data => {
-          if(data.message) {
-            console.error(data.message);
-            return [];
-          } else {
-            return Object.keys(data);
-          }
-        })
-      }
-      collection.addNode(node);
-    }
+    // Fetch repos from github api
+    const repos = await getRepos(GITHUB_USERNAME, process.env.PERSONAL_ACCESS_TOKEN, EXCLUDED_REPOS);
+    repos.forEach(collection.addNode);
   })
 
   api.createPages(({ createPage }) => {
