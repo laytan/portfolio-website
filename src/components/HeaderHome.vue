0f74784e4b987bc1e9152007559e92427c919a92
Laytan Laats <Laytanlaats@hotmail.com>
2020-03-02 20:14:50 +0100

    responsive header

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -20,7 +20,7 @@
     </div>
     <div class="header__3">
       <div>
-        <h2>Do Things Right</h2>
+        <h2>Do Things<br> Right</h2>
         <button class="btn btn--accent">
           About me
         </button>
@@ -48,10 +48,9 @@ nav {
 
 .header {
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-template-rows: min-content auto;
+  grid-template-columns: 1fr;
+  grid-template-rows: min-content repeat(8, 1fr);
   height: 100vh;
-  align-items: center;
 
   &__logo {
     display: flex;
@@ -59,61 +58,128 @@ nav {
     background: $background;
     padding: 1rem;
     box-shadow: 5px 5px 4px rgba(0,0,0,0.15);
+    font-size: 1rem;
 
     h1 {
       margin: 0;
     }
 
     img {
-      margin-right: 0rem;
-      max-height: 5.5rem;
+      margin-right: 1rem;
+      width: auto;
+      height: 4rem;
     }
   }
 
   > div {
-    grid-row: 2;
     font-size: 140%;
+    grid-column: 1;
   }
 
   &__1, &__3 {
-    height: 100%;
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    justify-content: center;
+    height: 100%;
+    
+    h2 {
+      text-align: center;
+      margin: 0 0 1rem 0;
+    }
 
     > div {
-      height: 35%;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-
-      h2 {
-        margin-bottom: 1rem;
-      }
+      width: 100%;
     }
   }
 
   &__1 {
-    grid-column-start: 1;
-    grid-column-end: 4;
     background-color: $accent;
+    align-items: flex-start;
     justify-content: flex-end;
+    grid-row-start: 2;
+    grid-row-end: 6;
+
+    > div {
+      margin-top: 2rem;
+    }
+  }
+
+  &__3 {
+    background-color: $primary;
+    justify-content: flex-start;
+    align-items: flex-end;
+    grid-row-start: 6;
+    grid-row-end: 10;
+
+    > div {
+      margin-bottom: 2rem;
+    }
   }
 
   &__2 {
-    grid-column-start: 3;
-    grid-column-end: 5;
+    grid-row-start: 5;
+    grid-row-end: 7;
     display: flex;
     justify-content: center;
     z-index: 2;
   }
+}
 
-  &__3 {
-    grid-column-start: 4;
-    grid-column-end: 7;
-    background-color: $primary;
-    justify-content: flex-start;
+@media(min-width: 359px) {
+  .header__logo {
+    font-size: 1.45rem;
+
+    img {
+      height: 5.5rem;
+    }
+  }
+}
+
+@media(min-width: 550px) {
+    .header__logo {
+    font-size: 1.75rem;
+
+    img {
+      height: 6.5rem;
+    }
+  }
+}
+
+@media(min-width: 1000px) {
+  .header {
+    grid-template-rows: min-content auto;
+    grid-template-columns: repeat(6, 1fr);
+    align-items: center;
+
+    > div {
+      grid-row: 2;
+    }
+
+    .header__1, .header__3 {
+      height: 100%;
+      flex-direction: column;
+      align-items: center;
+
+      > div {
+        height: 35%;
+        width: 100%;
+      }
+    }
+
+    .header__1 {
+      grid-column: 1 / 4;
+    }
+
+    .header__2 {
+      grid-column: 3 / 5;
+    }
+
+    .header__3 {
+      grid-column: 4 / 7;
+    }
   }
 }
 </style>
