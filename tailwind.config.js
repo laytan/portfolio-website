406e18c44f2f50426fd489b3c85da8f0040ac174
= <laytanlaats@hotmail.com>
2020-10-18 23:25:51 +0200

    basic theme and button styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,29 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
     purge: false,
+    theme: {
+        fontFamily: {
+            display: ['Archivo Black', 'Impact', 'sans-serif'],
+            body: ['Roboto', 'sans-serif'],
+        },
+        colors: {
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            blue: {
+                default: '#375fc0',
+                dark: '#284893',
+            },
+            pink: {
+                default: '#ff304f',
+                dark: '#db2944',
+            },
+        },
+        extend: {
+            spacing: {
+                'screen-4/5': '80vh',
+            }
+        }
+    },
 };
