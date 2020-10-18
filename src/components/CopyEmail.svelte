633a73a54cb0f6cbb779ebb11012602f69362209
= <laytanlaats@hotmail.com>
2020-10-18 23:26:18 +0200

    add svelte and get started on homepage

---
+++ src/components/CopyEmail.svelte
@@ -0,0 +1,11 @@
+<script>
+    let text = 'Copy Address';
+    const canCopy = Boolean(navigator.clipboard);
+    const copyEmail = () => {
+        navigator.clipboard.writeText('laytanlaats@hotmail.com').then(() => text = 'Copied');
+    };
+</script>
+
+{#if canCopy}
+    <button class="btn btn-pink" on:click={copyEmail}>{ text }</button>
+{/if}
---
+++ src/components/EnbiggenBox.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let className;
+</script>
+
+<div class="{`${className} w-0 w-full transition-transform duration-300 ease-in-out transform scale-y-0 md:scale-y-100 md:scale-x-0`}"></div>
